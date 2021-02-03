d377cb781a9b3f066b970626f289aa5926dde89d
João Mangueira <jamjs7@gmail.com>
2021-02-03 20:07:18 -0300

    configurando Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { MaterialIcons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
 
-import { Wrapper} from './styles';
+import colors from '../../styles/colors';
+import { Container, Avatar, OnlineStatus, RightSide, Button } from './styles';
 
 const Header: React.FC = () => {
     return (
-        <Wrapper>
-           dsDAS
-        </Wrapper>
+        <Container>
+            <Avatar>
+               <OnlineStatus />
+            </Avatar>
+
+            <RightSide>
+                <Button>
+                    <MaterialIcons
+                        name="notifications-none"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+
+                <Button>
+                    <MaterialCommunityIcons
+                        name="message-outline"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+
+                <Button>
+                    <Feather
+                        name="search"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+            </RightSide>
+        </Container>
     );
 };
 
