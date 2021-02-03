ddd9d58f03a79691070e454c1efeb75bfcf01be9
João Mangueira <jamjs7@gmail.com>
2021-02-03 11:52:00 -0300

    Criando pages ComingSoon e Following

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,15 +3,18 @@ import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import ComingSoon from './pages/ComingSoon';
+import Following from './pages/Following';
+
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const Routes: React.FC = () => (
   <NavigationContainer>
     <Navigator>
-      <Screen name="Following" component={View} />
-      <Screen name="Discover" component={View} />
-      <Screen name="Browse" component={View} />
-      <Screen name="Esports" component={View} />
+      <Screen name="Following" component={Following} />
+      <Screen name="Discover" component={ComingSoon} />
+      <Screen name="Browse" component={ComingSoon} />
+      <Screen name="Esports" component={ComingSoon} />
     </Navigator>
   </NavigationContainer>
 );
