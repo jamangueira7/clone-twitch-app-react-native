c2d6ea41e6e2cb1b1e9d426bb01650eb5b3b711b
João Mangueira <jamjs7@gmail.com>
2021-02-03 20:14:30 -0300

    estrutura para mostrar components na tela

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { View } from 'react-native';
 import Header from '../../components/Header';
 
 import { Wrapper, Container, Main } from './styles';
 
+interface Item {
+    key: string;
+    render: () => JSX.Element;
+    isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+    React.useMemo(() => {
+        const items: Item[] = [
+            {
+                key: 'PAGE_HEADING',
+                render: () => <View />
+            },
+            {
+                key: 'FOLLOWED_CATEGORIES',
+                render: () => <View />,
+                isTitle: true,
+            },
+            {
+                key: 'C1',
+                render: () => <View />,
+            },
+            {
+                key: 'LIVE_CHANNELS',
+                render: () => <View />,
+                isTitle: true,
+            },
+            {
+                key: 'C2',
+                render: () => <View />,
+            },
+            {
+                key: 'CONTINUE_WATCHING',
+                render: () => <View />,
+                isTitle: true,
+            },
+            {
+                key: 'C3',
+                render: () => <View />,
+            },
+            {
+                key: 'OFFLINE_CHANNELS',
+                render: () => <View />,
+                isTitle: true,
+            },
+            {
+                key: 'C4',
+                render: () => <View />,
+            }
+        ]
+    }, []);
     return (
 
         <Wrapper>
